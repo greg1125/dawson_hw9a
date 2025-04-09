0d364aa11ad3f0966beb66432b72b90984ebc1b2
Greg Dawson <retrop1125@gmail.com>
2025-04-08 17:59:35 -0700

    first release

---
+++ hw9a/ex3/index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Problem 6</title>
+</head>
+<a href="../index.html">Back to previous page</a>
+<body>
+    <h1>Answers to Problem 6</h1>
+    <p>
+        (a) The output is [4, 8, 12, 16, 20]. <br>
+
+        (b) The output is [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]. <br> 
+        
+        (c) The outputs differ because in (a) you filter first (keeping only even numbers) and then double them, <br>
+        while in (b) you double all numbers first (making them all even) and then filter, so you end up keeping every number. <br>
+    </p>
+</body>
+</html>
